d49ac3dcf93c48ff57ac1ba43148efe8a6601eff
samuelb20 <boury.samuel82@gmail.com>
2021-02-24 13:00:57 +0100

    making code cleaner & recovering real latest movies (changing API call)

--- lowtech/src/home/index.js
+++ lowtech/src/home/index.js
@@ -5,83 +5,65 @@ const homepage = require ('./homepage')
 
 // Fetch films
 // const filmsResponse = require("./seed")
-let numPageLat = [] 
-let numPagePopu = []
+let numPage = []
 
 const buildHomepage = async (categories, page) => {
   let films = []
-  let url
+  let currentDate = getCurrentDate()
+
+  if (page === "next") {
+    if (numPage[1] === 10) {
+      numPage[1] = 20
+    } else {
+      numPage[0] ++
+      numPage[1] = 10
+    }
+  } else if (page === "prev" && (numPage[0] > 1 | numPage[1] === 20)) {
+    if (numPage[1] === 10) {
+      numPage[0] -= 1
+      numPage[1] = 20
+    } else {
+      numPage[1] = 10
+    }
+  } else {
+    numPage.push(1)
+    numPage.push(10)
+  }
+
   for (index in categories) {
 
-    let real_cat
-    if (categories[index] === "popularity") {
-      real_cat = "Popular"
-      
-      if (page === "next") {
-        if (numPagePopu[1] === 10) {
-          numPagePopu[1] = 20
-        } else {
-          numPagePopu[0] ++
-          numPagePopu[1] = 10
-        }
-      } else if (page === "prev" && (numPagePopu[0] > 1 | numPagePopu[1] === 20)) {
-        if (numPagePopu[1] === 10) {
-          numPagePopu[0] -= 1
-          numPagePopu[1] = 20
-        } else {
-          numPagePopu[1] = 10
-        }
-      } else {
-        numPagePopu.push(1)
-        numPagePopu.push(10)
-      }
+    if (categories[index] === "Popular") {
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TOKEN}&sort_by=popularity.desc&page=${numPage[0]}`
     }
 
-    else if (categories[index] === "release_date") {
-      real_cat = "Latest"
-      
-      if (page === "next") {
-        if (numPageLat[1] === 10) {
-          numPageLat[0] --
-          numPageLat[1] = 20
-        } else {
-          numPageLat[1] = 10
-        }
-      } else if (page === "prev" && (numPageLat[0] < 500 | numPageLat[1] === 10)) {
-        if (numPageLat[1] === 10) {
-          numPageLat[1] = 20
-        } else {
-          numPageLat[0] ++
-          numPageLat[1] = 10
-        }
-      } else {
-        numPageLat.push(500)
-        numPageLat.push(20)
-      }
+    else if (categories[index] === "Latest") {
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TOKEN}&primary_release_date.gte=2020-06-01&primary_release_date.lte=${currentDate}&sort_by=primary_release_date.desc&page=${numPage[0]}`
     }
 
     else {
 
     }
     
-    let numPage, numPageDiv
-    numPage = categories[index] === "popularity" ? numPagePopu[0] : numPageLat[0]
-    numPageDiv = categories[index] === "popularity" ? numPagePopu[1] : numPageLat[1]
-    
-    url = `https://api.themoviedb.org/3/discover/movie?api_key=${TOKEN}&language=fr&sort_by=${categories[index]}.desc&page=${numPage}`
-
     try {
         let response = await fetch(url)           // wait for api fetch
         let responseJson = await response.json()  // wait for JSON parsing
-        films.push([real_cat, responseJson.results.slice(numPageDiv - 10, numPageDiv)])
+        films.push([categories[index], responseJson.results.slice(numPage[1] - 10, numPage[1])])
     } catch (error) {
         console.error(error);
     }
   }
-  
+
   return homepage(films)
 
 };
+
+function getCurrentDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  return yyyy+"-"+mm+"-"+dd
+}
 
 function pageManaging (category, numPage) {
 // Pour gérer la disjonction de cas au-dessus
