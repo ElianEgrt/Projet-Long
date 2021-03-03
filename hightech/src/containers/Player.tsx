2ca8c6e0c948babfb7f5bc461676e8c05bf0da68
samuelb20 <boury.samuel82@gmail.com>
2021-03-03 19:54:58 +0100

    beginning the movie page implementation

---
+++ hightech/src/containers/Player.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styled from "styled-components";
+
+const Player = () => {
+    return  <div>
+                <h2>Votre film :</h2>
+                <video src="" autoPlay={true} />
+            </div>
+};
+
+export default Player;
